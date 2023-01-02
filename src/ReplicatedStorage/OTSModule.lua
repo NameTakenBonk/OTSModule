923793dacd4f15aba40b7f17811c23d0cb263d2d
NameTakenBonk <83465599+NameTakenBonk@users.noreply.github.com>
2023-01-02 19:06:00 +0000

    Initial Release

--- src/ReplicatedStorage/OTSModule.lua
+++ src/ReplicatedStorage/OTSModule.lua
@@ -9,7 +9,7 @@ local RunService = game:GetService("RunService")
 local OTS = { }
 OTS.__index = OTS
 
-function OTS.new(Player : Player, Camera : Camera, Character, Humanoid, CameraOffset : Vector3)
+function OTS.new(Player : Player, Camera : Camera, Character, Humanoid, CameraOffset : Vector3, MouseIcon)
     local NewOTS = { }
     setmetatable(NewOTS, OTS)
 
@@ -31,44 +31,54 @@ function OTS.new(Player : Player, Camera : Camera, Character, Humanoid, CameraOf
     return NewOTS
 end
 
+-- // Enable Function
 function OTS:Enable()
+    -- // Checking if ots is already enabled
     if self.Enabled then return warn("OTS is already enabled!") end
-    self.Enabled = true
+    self.Enabled = true -- // Changing it to enabled
 
+    -- // Setting up the camera and mouse
     self.Camera.CameraType = Enum.CameraType.Scriptable
 
     UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
     UserInputService.MouseIconEnabled = false
 
+    -- // Changing the CameraAngleX and CameraAngleY when the mouse moves
     local function PlayerInput(ActionName, InputState, InputObject)
         if InputState == Enum.UserInputState.Change then
             self.CameraAngleX -= InputObject.Delta.X
-            self.CameraAngleY -= math.clamp(self.CameraAngleY - InputObject.Delta.Y * 0.4, -90, 90)
+            self.CameraAngleY -= math.clamp(self.CameraAngleY - InputObject.Delta.Y * 0.4, -90, 90) -- // Clamping the Y rotation(Currently needs fixing)
         end
     end
 
+    -- // Binding the PlayerInput when the mouse moves
     ContextActionService:BindAction("PlayerInput", PlayerInput, false, Enum.UserInputType.MouseMovement, Enum.UserInputType.Touch)
 
+    -- // Alligning the HumanoidRootPart and Setting the camera position
     self.RenderStepped = RunService.RenderStepped:Connect(function()
-        local StartCFrame = CFrame.new(self.HumanoidRootPart.CFrame.Position) * CFrame.Angles(0, math.rad(self.CameraAngleX), 0) * CFrame.Angles(0, math.rad(self.CameraAngleY), 0)
-        local CameraCFrame = StartCFrame:PointToWorldSpace(self.CameraOffset)
-        local CameraFocus = StartCFrame:PointToWorldSpace(Vector3.new(self.CameraOffset.X, self.CameraOffset.Y, -100000))
+        local StartCFrame = CFrame.new(self.HumanoidRootPart.CFrame.Position) * CFrame.Angles(0, math.rad(self.CameraAngleX), 0) * CFrame.Angles(0, math.rad(self.CameraAngleY), 0) -- // Making the starting CFrame
+        local CameraCFrame = StartCFrame:PointToWorldSpace(self.CameraOffset) -- // Making the camera position
+        local CameraFocus = StartCFrame:PointToWorldSpace(Vector3.new(self.CameraOffset.X, self.CameraOffset.Y, -100000)) -- // Making the camera dirction
 
 		self.Camera.CFrame = CFrame.lookAt(CameraCFrame, CameraFocus)
 		
-		local LookCFrame = CFrame.lookAt(self.HumanoidRootPart.Position, self.Camera.CFrame:PointToWorldSpace(Vector3.new(0, 0, -100000)))
-        self.HumanoidRootPart.CFrame = CFrame.fromMatrix(self.HumanoidRootPart.Position, LookCFrame.XVector, self.HumanoidRootPart.CFrame.YVector)
+		local LookCFrame = CFrame.lookAt(self.HumanoidRootPart.Position, self.Camera.CFrame:PointToWorldSpace(Vector3.new(0, 0, -100000))) -- // the general direction where the camera is looking at
+        self.HumanoidRootPart.CFrame = CFrame.fromMatrix(self.HumanoidRootPart.Position, LookCFrame.XVector, self.HumanoidRootPart.CFrame.YVector) -- // Setting the humanoid rotation to the camera
     end)
 end
 
 function OTS:Disable()
+    -- // Checking if ots is already disabled
     if not self.Enabled then return warn("OTS is already disabled!") end
+    self.Enabled = false -- // Setting the ots to disabled
 
+    -- // Changing the camera back to defualt
     self.Camera.CameraType = Enum.CameraType.Custom
 
     UserInputService.MouseBehavior = Enum.MouseBehavior.Default
     UserInputService.MouseIconEnabled = true
 
+    -- // Disconnecting the Allignment and positioning
     self.RenderStepped:Disconnect()
 end
 
